906f48bf7c8dc53ca8ca9fa1e377f2b8e5f0e91c
saigunaraj <rajas0465@gmail.com>
2024-11-05 02:28:38 +0530

    28th

---
+++ scripts/install_api_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the API directory
+cd /var/www/api
+
+# Install npm dependencies
+npm install
---
+++ scripts/install_expo_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the RMD directory
+cd /var/www/rmd
+
+# Install npm dependencies
+npm install
