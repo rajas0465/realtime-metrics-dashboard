01a3a8c5aaae129c09a117bda9b3e8d789470ad0
saigunaraj <rajas0465@gmail.com>
2024-11-06 00:58:58 +0530

    36

--- scripts/start_expo.sh
+++ scripts/start_expo.sh
@@ -6,7 +6,7 @@ cd /var/www/rmd  # Adjust the path as needed
 sudo chmod +x /var/www/rmd/node_modules/.bin/expo
 
 # Start the Expo app using pm2 with Expo CLI
-pm2 start "npx expo start" --name "expo-app"
+pm2 start "npx expo start" --name "expo-app2"
 
 # Save the PM2 process list to ensure it restarts on reboot
 pm2 save
