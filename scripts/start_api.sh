29cdd2d8a423d91e20860ca65e3997bb99242c65
saigunaraj <rajas0465@gmail.com>
2024-11-09 23:50:15 +0530

    63

--- scripts/start_api.sh
+++ scripts/start_api.sh
@@ -5,7 +5,7 @@ cd /var/www/api  # Adjust the path as needed
 
 #pm2 stop "api"
 # Start the API using pm2
-pm2 restart app.js --name "api"  # Replace 'server.js' with your API entry point file
+/home/ec2-user/.nvm/versions/node/v22.11.0/bin/pm2 restart app.js --name "api"  # Replace 'server.js' with your API entry point file
 
 # Save the PM2 process list to ensure it restarts on reboot
 pm2 save
